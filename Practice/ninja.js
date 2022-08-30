dc258f8b809ec489bcf04e83028625a24aeb1783
EricD1996 <EricD8816051@gmail.com>
2022-08-30 00:55:26 -0700

    upload

--- Practice/ninja.js
+++ Practice/ninja.js
@@ -22,7 +22,29 @@ class Ninja {
     }
 }
 
+class Sensei extends Ninja{
+    constructor(name){
+        super(name);
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    speakWisdom(){
+        console.log(this.name);
+        console.log(this.health);
+        console.log(this.speed);
+        console.log(this.strength);
+        console.log(this.wisdom);
+        this.drinkSake();
+        console.log("Hello")
+    }
+}
 const ninja1 = new Ninja("Hyabusa",13);
 ninja1.drinkSake();
 ninja1.sayName();
 ninja1.showStats();
+
+const sensei1 = new Sensei("sensei");
+sensei1.speakWisdom();
